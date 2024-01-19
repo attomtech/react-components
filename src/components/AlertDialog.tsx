d4f6827fbfd980e8806abff8bd22298edf0076a8
Paulo Cezar Motta <pmotta@lumis.com.br>
2024-01-19 13:56:18 -0300

    Migrando componente AlertDialog para TailwindCSS

--- src/components/AlertDialog.tsx
+++ src/components/AlertDialog.tsx
@@ -1,12 +1,5 @@
 import * as AlertDialogRadix from '@radix-ui/react-alert-dialog'
-import {
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogOverlay,
-  AlertDialogTitle,
-  Flex
-} from './styles'
-import { Button, ButtonProps } from '../Button'
+import { Button, ButtonProps } from './Button'
 import { forwardRef, useImperativeHandle, useState } from 'react'
 
 export interface AlertDialogProps {
@@ -42,19 +35,34 @@ export const AlertDialog = forwardRef(
     return (
       <AlertDialogRadix.Root open={open} onOpenChange={setOpen}>
         <AlertDialogRadix.Portal>
-          <AlertDialogOverlay />
-          <AlertDialogContent>
-            <AlertDialogTitle>{titulo}</AlertDialogTitle>
-            <AlertDialogDescription>{descricao}</AlertDialogDescription>
-            <Flex>
+          <AlertDialogRadix.Overlay className="bg-black opacity-50 fixed inset-0 data-[state=open]:animate-overlayShow" />
+          <AlertDialogRadix.Content
+            className={`
+            bg-zinc-900 
+            rounded-md 
+            fixed top-1/2 left-1/2 
+            translate-x-[-50%] translate-y-[-50%]
+            shadow-[hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px],
+            w-[90vw] max-w-[500px] max-h-[85vh]
+            p-6
+            data-[state=open]:animate-contentShow
+            focus:outline-none`}
+          >
+            <AlertDialogRadix.Title className="m-0 text-zinc-100 text-2xl font-bold">
+              {titulo}
+            </AlertDialogRadix.Title>
+            <AlertDialogRadix.Description className="mb-5 text-zinc-100 text-base leading-3">
+              {descricao}
+            </AlertDialogRadix.Description>
+            <div className="flex justify-end gap-3">
               <Button variant="back" onClick={() => setOpen(false)}>
                 Cancelar
               </Button>
               <Button variant={variant} onClick={onConfirmHandle}>
                 {buttonText}
               </Button>
-            </Flex>
-          </AlertDialogContent>
+            </div>
+          </AlertDialogRadix.Content>
         </AlertDialogRadix.Portal>
       </AlertDialogRadix.Root>
     )
