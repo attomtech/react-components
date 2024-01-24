213f41cdbbbdf643f2c5f153dde5092e33157624
Paulo Cezar Motta <pmotta@lumis.com.br>
2024-01-24 11:01:55 -0300

    Corrigindo estilo do component Button

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -8,7 +8,7 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function Button({
   children,
-  className,
+  className = '',
   variant = 'default',
   size = 'md',
   ...props
@@ -19,7 +19,7 @@ export function Button({
       data-size={size}
       className={`
           rounded-md 
-          text-sm text-center 
+          text-sm text-center text-white
           font-medium 
           min-w-32 
           box-border 
@@ -27,18 +27,19 @@ export function Button({
           flex items-center justify-center gap-2 
           cursor-pointer 
           [&>svg]:w-4 [&>svg]:h-4 
-          disabled:cursor-not-allowed disabled:bg-zinc-200 
+          disabled:cursor-not-allowed disabled:bg-zinc-200 disabled:text-zinc-800
           focus:shadow focus:shadow-zinc-100
           
-          primary:text-white primary:bg-sky-500 primary:enabled:hover:bg-sky-300 
-          success:text-white success:bg-emerald-500 success:enabled:hover:bg-emerald-300
-          warning:text-white warning:bg-yellow-500 warning:enabled:hover:bg-yellow-300
-          danger:text-white danger:bg-red-500 danger:enabled:hover:bg-red-300
-          default:text-zinc-100 default:enabled:hover:text-white default:disabled:text-zinc-600 default:disabled:bg-transparent
+          primary:enabled:bg-sky-500 primary:enabled:hover:bg-sky-300 
+          success:enabled:bg-emerald-500 success:enabled:hover:bg-emerald-300
+          warning:enabled:bg-yellow-500 warning:enabled:hover:bg-yellow-300
+          danger:enabled:bg-red-500 danger:enabled:hover:bg-red-300
+          default:enabled:enabled:text-zinc-100 default:enabled:hover:text-white default:disabled:text-zinc-600 default:disabled:bg-transparent
           
           data-[size=sm]:h-9
           data-[size-md]:h-11
           data-[size-lg]:h-14
+          ${className}
       `}
       {...props}
     >
